15e61eced28a902f0f5455c38405312ec169a4c7
Avik Das <119071924+AvikAB@users.noreply.github.com>
2023-02-04 22:27:30 +0600

    Update MyCode.c

--- MyCode.c
+++ MyCode.c
@@ -1016,4 +1016,19 @@ int main()
     printf("%d\n",determinant);
 }
 
+// DIVISORS Problem:
+
+#include<stdio.h>
+int main()
+{
+	int x, i;
+	scanf("%d", &x);
+	for(i=1; i<=x; i++) {
+		if((x%i)==0){
+			printf("%d", i);
+			printf("\n");
+		}
+	}
+}
+
 
